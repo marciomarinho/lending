f34c048dd47c5e82208634e03a0260816f8953dd
ManoogD <ManoogianDavid@gmail.com>
2016-03-27 23:47:42 +0400

    doesnt find how to fix delete in test

--- src/test/java/au/com/riosoftware/BookControllerIntegrationTest.java
+++ src/test/java/au/com/riosoftware/BookControllerIntegrationTest.java
@@ -1,9 +1,12 @@
 package au.com.riosoftware;
 
 
+import au.com.riosoftware.domain.Book;
+import au.com.riosoftware.services.BookService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -12,10 +15,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.mockito.Mockito.when;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = LendingApplication.class)
@@ -23,6 +29,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class BookControllerIntegrationTest {
 
     private MockMvc mockMvc;
+
+    @Mock
+    private BookService bookService;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -48,9 +57,11 @@ public class BookControllerIntegrationTest {
 
     @Test
     public void showDeleteBookForm() throws Exception{
-        mockMvc.perform(get("/books/{id}/delete"))
+        when(bookService.findOne(1)).thenReturn(new Book());
+        mockMvc.perform(get("/books/{id}/delete", 1))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/books"));
+
     }
 
     @Test
